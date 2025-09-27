d0fac552df352939c5237bab86c3489225364ac3
vaalss <valeria.hernandezmaldo@gmail.com>
2025-09-27 15:34:49 -0600

    Métodos Jugador

--- EJERCICIOS/03 - Ejercicio 4/Jugador.java
+++ EJERCICIOS/03 - Ejercicio 4/Jugador.java
@@ -5,4 +5,16 @@ public abstract class Jugador extends Combatiente {
         super(nombre, vidaTotal, ataque);
         this.inventario = new ArrayList<>();
     }
+
+    public void usarItem(Item item, Combatiente objetivo) {
+        item.aplicarEfecto(objetivo);
+        inventario.remove(item); //para que no se usa más de una vez
+    }
+
+    public abstract int elegirAccion();
+    public abstract Combatiente elegirObjeto(List<Combatiente> posibles);
+
+    public List<Item> getInventario() {
+        return this.inventario;
+    }
 }
