30d57efc5d740987d0a24ca7cd2dee9ddd7728e5
vaalss <valeria.hernandezmaldo@gmail.com>
2025-09-28 18:52:45 -0600

    Correción constructor clase Guerrero y Explorador

---
+++ EJERCICIOS/03 - Ejercicio 4/Explorador.java
@@ -0,0 +1,5 @@
+public class Explorador extends Jugador { //menos vida y ataque, más items (5)
+    public Explorador (String nombre, int vidaTotal, int ataque) {
+        super(nombre, vidaTotal, ataque, "Explorador");
+    }
+}
--- EJERCICIOS/03 - Ejercicio 4/Guerrero.java
+++ EJERCICIOS/03 - Ejercicio 4/Guerrero.java
@@ -1,6 +1,6 @@
-public class Guerrero extends Jugador {
-    
-    public Guerrero(String nombre, int vidaTotal, int ataque) {
-        super(nombre, vidaTotal, ataque);
+public class Guerrero extends Jugador { //mas vida y ataque, menos items (2)
+    public Guerrero (String nombre, int vidaTotal, int ataque) {
+        super(nombre, vidaTotal, ataque, "Guerrero");
     }
+
 }
