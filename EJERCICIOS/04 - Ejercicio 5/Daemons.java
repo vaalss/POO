d81e1a081496a6d6c38b0a8ca9eecd054b9e6c1e
vaalss <valeria.hernandezmaldo@gmail.com>
2025-10-03 16:15:32 -0600

    clase Daemons

---
+++ EJERCICIOS/04 - Ejercicio 5/Daemons.java
@@ -0,0 +1,44 @@
+import java.time.LocalTime;
+
+public class Daemons extends Process {
+    private boolean activo;
+
+    public Daemons(String nombre) {
+        super(nombre);
+        this.activo = true;
+    }
+
+    public String ejecutar() {
+            String tipo = this.getNombre();
+            switch (tipo) {
+                case "Monitoreo":
+                    while (activo) {
+                        try {
+                            System.out.println("Proceso Daemon activo, monitoreando procesos...");
+                            Thread.sleep(5000); // espera 5 segundos
+                        } catch (InterruptedException e) {
+                            return e.getMessage();
+                        }
+                    }
+                    return "Monitoreo detenido.";
+                case "Hora Actual":
+                    LocalTime hora = LocalTime.now();
+                    try {
+                        Thread.sleep(5000); // espera 5 segundo
+                    } catch (InterruptedException e) {
+                        return e.getMessage();
+                    }
+                    return "La hora actual es " + hora;
+                case "Apagar sistema":
+                    this.activo = false;
+                    return "Apagando el sistema..."
+                default:
+                    return "No se reconoce el proceso";
+            }
+    }
+
+    @Override 
+     public String toString() {
+        return "Proceso de Daemons: " + this.getPID() + " '" + this.getNombre + "'";
+    }
+}
