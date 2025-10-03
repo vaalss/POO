564cc2c319e14cf44f774a7db4333e2e9e22cda1
vaalss <valeria.hernandezmaldo@gmail.com>
2025-10-03 16:29:04 -0600

    clase View

---
+++ EJERCICIOS/04 - Ejercicio 5/View.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class View {
+    private Scanner sc;
+
+    public View() {
+        this.sc = new Scanner(System.in);
+    }
+
+    public void mostrarMenu() {
+        print("--- MENU ---");
+        println("   1. Correr procesos");
+        println("   2. Salir");
+    }
+
+    public int pedirNumero(String prompt) {
+        print(prompt);
+        int entrada = sc.nextInt();
+        sc.nextLine();
+        return entrada;
+    }
+
+    public String pedirEntrada(String prompt) {
+        print(prompt);
+        String entrada = sc.nextLine();
+        return entrada;
+    }
+
+    public void mostrarProcesos(ArrayList<Process> procesos) {
+        println("--- Procesos --- \n")
+        for (Process p : procesos) {
+            println(p);
+            println(p.ejecutar());
+        }
+    }
+
+
+    //helpers
+    public void print(String s) {
+        System.out.print(s);
+    }
+
+    public void println(String s) {
+        System.out.println(s);
+    }
+}
