90e0bb30d2cfa4d1f10259c9773a1875ee06f63a
vaalss <valeria.hernandezmaldo@gmail.com>
2025-10-10 19:02:02 -0600

    clase Enfermero

---
+++ LABORATORIOS/02 - LAB 3/Enfermero.java
@@ -0,0 +1,32 @@
+public class Enfermero extends Medico {
+    public String turno;
+    public String nivel;
+    public double bonificacion;
+
+    public Enfermero(String turno, String nivel, double bonificacion) {
+        super(nombre, experiencia, salarioBase, disponible);
+        this.turno = turno;
+        this.nivel = nivel;
+        this.bonificacion = bonificacion;
+    }
+
+    public String getTurno() {
+        return this.turno;
+    }
+
+    public String getNivel() {
+        return this.nivel;
+    }
+
+    public double gerBonificacion() {
+        return this.bonificacion;
+    }
+
+    public boolean bonificacionActiva() {
+        if (this.tirno.equals("NOCHE")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
