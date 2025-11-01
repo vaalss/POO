7af23dc6dba0a05178c2a7d0e4fefb5f53b5772c
vaalss <valeria.hernandezmaldo@gmail.com>
2025-11-01 14:00:26 -0600

    corrección de implementación y toString

--- EJERCICIOS/05 - Ejercicio 6/Drone.java
+++ EJERCICIOS/05 - Ejercicio 6/Drone.java
@@ -1,17 +1,19 @@
 public class Drone extends Equipment implements IMedible, IRegistrar {
 
+    private String medicion, registro;
+
     public Drone(String nombre, double consumo) {
         super(nombre, consumo);
     }
 
     @Override
-    public String realizarMedicion(String medicion) {
-        return  "Realiza la siguiente medición: \n" + medicion;
+    public void realizarMedicion(String medicion) {
+        this.medicion = "Realiza la siguiente medición: \n" + medicion;
     }
 
     @Override
-    public String realizarRegistro(String registro) {
-        return "Realiza el siguiente registro: \n" + registro;
+    public void realizarRegistro(String registro) {
+        this.registro = "Realiza el siguiente registro: \n" + registro;
     }
 
     @Override
@@ -28,6 +30,6 @@ public class Drone extends Equipment implements IMedible, IRegistrar {
      @Override
     public String toString() {
         return "Dron #" + this.ID + ": \n- Nombre: " + this.nombre + "\n- Consumo de energía: " + this.comsumo +
-        "\n- Capacidades: \n  - " + "\n  - " + this.realizarMedicion() + "\n  - " + this.realizarRegistro();  
+        "\n- Capacidades: \n  - " + "\n  - " + this.medicion + "\n  - " + this.registro;  
     }
 }
