a8b7de8696f52ca6b4353cad3469e1f4f383dafd
vaalss <valeria.hernandezmaldo@gmail.com>
2025-11-01 18:54:27 -0600

    atributos y constructor clase Controller

---
+++ EJERCICIOS/05 - Ejercicio 6/Controller.java
@@ -0,0 +1,14 @@
+public class Controller {
+    
+    private ArrayList<Equipment> equipos;
+    private View vista;
+
+    public Controller() {
+        this.equipo = new ArrayList<Equipment>();
+        this.vista = new View();
+    }
+
+    public init() {
+
+    }
+}
