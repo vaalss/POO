598e1bd0b979521c02877607d1c9f97ad0d2c25d
vaalss <valeria.hernandezmaldo@gmail.com>
2025-11-02 14:49:23 -0600

    método principal controlador

--- EJERCICIOS/05 - Ejercicio 6/Controller.java
+++ EJERCICIOS/05 - Ejercicio 6/Controller.java
@@ -11,4 +11,30 @@ public class Controller {
     public init() {
 
     }
+
+    public void sistema() {
+        int salir = -1;
+        while (salir == -1) {
+            vista.mostrarMenu();
+            int opcion = vista.pedirNumero("Selecciona la opción a realizar: ");
+            switch (opcion) {
+                case 1:
+                    vista.mostrarEquipo(equipos);
+                    break;
+                case 2:
+                    String equipo = vista.pedirEntrada("Ingresa el nombre o ID del equipo que desees: ");
+                    vista.buscarEquipo(equipos, equipo);
+                    break;
+                case 3:
+                    Collections.sort(equipos);
+                    vista.equiposPorConsumo(equipos);
+                    break;
+                case 4:
+                    salir = 0;
+                    break;
+                default:
+                    vista.mostrarMensaje("Opción inválida");
+            }   
+        }
+    }
 }
